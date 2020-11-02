797b945ee93b286f3d2c8035c7d4260ddab4fd24
Robert Gordon <Komeuppance@gmail.com>
2020-11-02 13:43:44 -0800

    Initial setup

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+//Author Struct
+type Author struct {
+	Firstname string `json:"firstname`
+	Lastname  string `json:"lastname`
+}
+
+//Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Create New Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	//Init Router
+	r := mux.NewRouter()
+
+	//Route Handlers/Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
